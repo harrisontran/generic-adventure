bbfc336d581a9d2e6dfa1072b17a5c03e6a9acda
Harrison Tran <atxharrison.tran@gmail.com>
2017-04-22 15:55:25 -0500

    VS2017 libs and SDL
    Get some groundwork done on the actual game GUI itself. I think I'm
    gonna roll with SDL2 but we'll see what happens.

---
+++ vs/LifeOfEgbert/LifeOfEgbert/stdafx.h
@@ -0,0 +1,7 @@
+// Include Simple DirectMedia Layer Libraries
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+// Some other libraries that we might want, for debugging and other purposes
+#include <iostream>
